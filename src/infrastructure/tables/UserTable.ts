7b9aa5beccf19ec0659e77267a07c56e8db8e976
Danil <danil_ustinov@mail.ru>
2025-06-26 09:36:46 +0700

    fix: remove implements IDeepPartial, use extends

--- src/infrastructure/tables/UserTable.ts
+++ src/infrastructure/tables/UserTable.ts
@@ -1,6 +1,5 @@
 import {UserModel} from "../../domain/models/UserModel";
-import {IDeepPartial} from "@steroidsjs/nest/usecases/interfaces/IDeepPartial";
 import {TypeOrmTableFromModel} from '@steroidsjs/nest/infrastructure/decorators/typeorm/TypeOrmTableFromModel';
 
 @TypeOrmTableFromModel(UserModel, 'user')
-export class UserTable implements IDeepPartial<UserModel> {}
+export class UserTable extends UserModel {}
