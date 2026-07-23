83a9247b4d49717b75bfe9ba8191196dd1a62440
Vladislav Koktashev <vladislav54415@gmail.com>
2026-07-23 12:55:13 +0300

    feat: #276 replace typeorm forks with native packages

--- src/infrastructure/repositories/UserRepository.ts
+++ src/infrastructure/repositories/UserRepository.ts
@@ -1,9 +1,9 @@
-import {Repository} from '@steroidsjs/typeorm';
+import {Repository} from 'typeorm';
 import {Injectable} from '@nestjs/common';
-import {InjectRepository} from '@steroidsjs/nest-typeorm';
-import {CrudRepository} from "@steroidsjs/nest/infrastructure/repositories/CrudRepository";
-import {UserModel} from "../../domain/models/UserModel";
-import {UserTable} from "../tables/UserTable";
+import {InjectRepository} from '@nestjs/typeorm';
+import {CrudRepository} from '@steroidsjs/nest/infrastructure/repositories/CrudRepository';
+import {UserModel} from '../../domain/models/UserModel';
+import {UserTable} from '../tables/UserTable';
 
 @Injectable()
 export class UserRepository extends CrudRepository<UserModel> {
