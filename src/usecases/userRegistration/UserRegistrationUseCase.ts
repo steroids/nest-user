b5e6a1c49da0f75fa0200ffea44763602c5e9fcd
Vladislav Koktashev <vladislav54415@gmail.com>
2026-08-13 12:54:15 +0300

    fix: use IUserService type in UserRegistrationUseCase

--- src/usecases/userRegistration/UserRegistrationUseCase.ts
+++ src/usecases/userRegistration/UserRegistrationUseCase.ts
@@ -1,14 +1,14 @@
 import { DataMapper } from '@steroidsjs/nest/usecases/helpers/DataMapper';
 import { ISessionService } from '@steroidsjs/nest-modules/auth/services/ISessionService';
 import { IUserRegistrationUseCase } from '@steroidsjs/nest-modules/user/usecases/IUserRegistrationUseCase';
+import { IUserService } from '@steroidsjs/nest-modules/user/services/IUserService';
 import { UserModel } from '../../domain/models/UserModel';
 import { UserRegistrationDto } from './dtos/UserRegistrationDto';
-import { UserService } from '../../domain/services/UserService';
 
 export class UserRegistrationUseCase implements IUserRegistrationUseCase {
     constructor(
         private readonly sessionService: ISessionService,
-        private readonly userService: UserService,
+        private readonly userService: IUserService,
     ) {}
 
     public async handle(dto: UserRegistrationDto) {
